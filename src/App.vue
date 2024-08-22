32d2a5ae041a27ea4f16d58cc36173472a3b5057
Lucas MICHEL <lucas.michel52240@gmail.com>
2024-08-22 14:35:37 +0200

    menu burger bootstrap

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,25 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
   </header>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-        <RouterLink class="nav-link" to="/" exact-active-class="active">Recherche d'entreprise</RouterLink>
-        <RouterLink class="nav-link" to="/entreprises" exact-active-class="active">Liste des entreprises</RouterLink>
-      </div>
-    </div>
-  </div>
-</nav>
 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/" exact-active-class="active">Recherche d'entreprise</RouterLink>
+
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/entreprises" exact-active-class="active">Liste des entreprises</RouterLink>
+
+            </li>
+          </ul>
+        </div>
+      </nav>
   <RouterView />
 </template>
 
